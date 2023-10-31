2f3365288bfb8eedfdead85145397aa5ccf1a6da
Dmitry <dmitryivanov92@gmail.com>
2023-10-31 10:43:04 +0400

    complete day6 tasks

---
+++ src/Day6/Task3.java
@@ -0,0 +1,10 @@
+package Day6;
+
+public class Task3 {
+    public static void main(String[] args){
+        Teacher geoTeacher = new Teacher("Иван Ларионов", "Геометрия");
+        Student vasil = new Student("Василий Круглов");
+        geoTeacher.evaluate(vasil
+        );
+    }
+}
---
+++ src/Day6/Teacher.java
@@ -0,0 +1,29 @@
+package Day6;
+
+import java.util.Random;
+
+public class Teacher {
+    private final String name;
+    private final String subject;
+
+    public Teacher(String name, String subject) {
+        this.name = name;
+        this.subject = subject;
+    }
+
+    public void evaluate(Student student) {
+        String mark = switch (new Random().nextInt(4) + 2) {
+            case 2 -> "неудовлетворительно";
+            case 3 -> "удовлетворительно";
+            case 4 -> "хорошо";
+            case 5 -> "отлично";
+            default -> "";
+        };
+
+        System.out.println("Преподаватель " + this.name + " оценил студента с именем " + student.getName()
+                + " по предмету " + this.subject + " на оценку " + mark + ".");
+
+
+    }
+
+}
