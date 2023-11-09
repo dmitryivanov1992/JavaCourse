55001bed51e4d5cb4d99f3ef89fafc6a23dc5389
Dmitry Ivanov <dmitryivanov92@gmail.com>
2023-11-09 23:42:17 +0400

    complete day15 task

---
+++ src/Day15/Task1.java
@@ -0,0 +1,36 @@
+package Day15;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        File fileRead = new File("src/main/resources/shoes.csv");
+
+        try {
+            Scanner scanner = new Scanner(fileRead);
+            PrintWriter writer = new PrintWriter("src/main/resources/missing_shoes.txt", StandardCharsets.UTF_8);
+
+            while (scanner.hasNext()) {
+                String lineString = scanner.nextLine();
+                if (!lineString.contains(";")) throw new IOException("Некорректный входной файл");
+                String[] line = lineString.split(";");
+                if (line.length != 3) throw new IOException("Некорректный входной файл");
+                if (Integer.parseInt(line[2]) == 0) {
+                    writer.println(line[0] + ", " + line[1] + ", " + line[2]);
+                }
+            }
+
+            scanner.close();
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
